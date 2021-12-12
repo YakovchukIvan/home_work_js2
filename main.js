b5566defc90c76b09b9c363a85432aa257194369
Ivan Yakovchuk <litvinovjobs@gmail.com>
2021-12-12 19:31:33 +0200

    home_work_js2

---
+++ main.js
@@ -0,0 +1,131 @@
+// Task1
+
+// question = +prompt ("Please enter your age");
+
+// if (question > 0 && question < 15) {
+//     console.log("Child");
+// }
+// else if (question > 14 && question < 19) {
+//     console.log("Teenager");
+// }
+// else if (question > 18 && question < 25) {
+//     console.log("Young man");
+// }
+// else if (question > 24 && question < 55) {
+//     console.log("Adult man");
+// }
+// else if (question > 54 && question < 80) {
+//     console.log("An old man");
+// }
+// else 
+//     console.log("Very old man");
+
+// Task2
+
+// question = +prompt ("Please enter a number between 0-9");
+
+// if (question == 0) {
+//     console.log (")");
+// }
+// else {
+//     if (question == 1) {
+//         console.log ("!");
+//     }
+//     else {
+//         if (question == 2) {
+//             console.log ("@");
+//         }
+//         else {
+//             if (question == 3) {
+//                 console.log ("#");
+//             }
+//             else {
+//                 if (question == 4) {
+//                     console.log ("$");
+//                 }
+//                 else {
+//                     if (question == 5) {
+//                         console.log ("%");
+//                     }
+//                     else {
+//                         if (question == 6) {
+//                             console.log ("^");
+//                         }
+//                         else {
+//                             if (question == 7) {
+//                                 console.log ("&");
+//                             }
+//                             else {
+//                                 if (question == 8) {
+//                                     console.log ("*");
+//                                 }
+//                                 else {
+//                                     if (question == 9) {
+//                                         console.log ("(");
+//                                     }
+//                                     else {
+//                                         console.log ("Invalid number");
+//                                     }
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// Task3
+
+// number = +prompt( 'Enter a three-digit number: ');
+// number1 = parseInt(number / 100);
+// number2 = parseInt(number / 10) % 10;
+// number3 = number % 10;
+// if(number1==number2 || number2==number3 || number1==number3)
+// alert('Найдено совпадение цифр');
+// else
+// alert('Совпадений не найдено');
+
+// Task4
+
+// Option 1
+// year = +prompt( "Enter year of birth: ");
+// if (year % 4 == 0) and (year % 100 != 0) && (year % 400 == 0) {
+//     alert ('YES');
+// }
+    
+// else {
+//     alert ('NO');
+// }
+    
+// Option 2
+// let year =+prompt ("Enter year of birth: ");
+
+// if (year % 4 ==0 || year % 100 != 0 && year % 400 == 0) {
+//     alert ('Yes');
+// }
+// else {
+//     alert ('No');
+// }
+
+// Task5
+
+
+
+// Task6
+
+// let number = +prompt("Введіть будь ласка кількість USD: ");
+// let reply = +prompt ("Введіть валюту яку хочете отримати: EUR, UAN, AZN");
+// // let USD = 27;
+// let EUR = (0,88);
+// let UAN = (27);
+// let AZN = (1,70);
+
+// if (reply = EUR) {
+//     let result = (number * EUR);
+//     console.log(`Total Price: ${result}$`);
+// }
+
+// TEST
+
